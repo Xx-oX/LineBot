9417e6bf495054a43eed655ead07dd331950079e
Xx-oX <ycwu4142@gmail.com>
2020-12-21 21:12:55 +0800

    fuck this shit

--- utils.py
+++ utils.py
@@ -4,9 +4,8 @@ from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, FlexSendMessage, ImageSendMessage
 
 
-
-channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
-print(channel_access_token)
+channel_access_token = "ge4ebr1e5XBnqmlFogeV8grB8JzZTXhslMcZ6sDqzr/TxTmurm8YKfGsIa+Lt4tXXro0LIvzoRuzv1PU3VwTq7nSipl2zn0LL6aJYI92qu81YP4I1u+5E2GbG4bLnGp1Ee7xhMKrEkLDZVm6i1alagdB04t89/1O/w1cDnyilFU="
+# channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
 
 def send_text_message(reply_token, text):
     line_bot_api = LineBotApi(channel_access_token)
